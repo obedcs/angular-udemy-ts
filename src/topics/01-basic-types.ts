a7ad180e745cf17d7ed05701e4500219b776a910
Obed <obed_cs@hotmail.com>
2024-07-05 12:35:16 -0600

    primer commit

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,12 @@
+const name: string = 'Obed';
+let variable2: number | string = 123;
+const isAlive: boolean = true;
+
+variable2 = 'hola'
+
+console.log({
+    name, variable2, isAlive
+});
+
+
+export {};
---
+++ src/topics/03-functions.ts
@@ -0,0 +1,14 @@
+function addNumbers( a: number, b: number ) {
+    return a+b;
+}
+
+const addNumbersArrow = ( a: number, b: number ): string => {
+    return (a+b).toString(); 
+}
+
+const result = addNumbers(1, 2);
+
+console.log({result});
+
+
+export {};
